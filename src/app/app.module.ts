157096deaf706145cf599d824b904506e50ad29a
Chris Griffith <chris.griffith@gmail.com>
2017-04-17 06:25:58 -0700

    Updating for Ionic 3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,6 @@
 import { NgModule, ErrorHandler } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpModule } from '@angular/http';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 import { StatusBar } from '@ionic-native/status-bar';
@@ -12,6 +14,8 @@ import { TaskListPage } from '../pages/tasklist/tasklist';
     TaskListPage
   ],
   imports: [
+    BrowserModule,
+    HttpModule,
     IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
